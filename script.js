598a2bc086064fbe422ca6c7e98066c5f78e426e
Gorakh Raj joshi <gorakh626@gmail.com>
2022-06-02 05:58:42 +0545

    Functions & Hoisting

---
+++ script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// Function Declaration
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+
+// console.log(sum);
+// console.log(myCountry);
+
+// Function Expression
+const sum = function (a, b) {
+  console.log("Running from function expression");
+  console.log(a + b);
+};
+sum(2, 3);
+
+// var myCountry = "Nepal";
+
+// Arrow Function
+// const sum = (a = 0, b = 0, c = 0) => {
+//   return a + b + c;
+// };
+
+// function sum() {
+//   return console.log("Function Declaration");
+// }
+
+// const sum = [1, 2, 3, 4];
+
+// sum(); //Arguments
+
+// console.log(sum);
+
+// Calling functiion
+// Running Function
+// Invoking Function
+
+// console.log(sum(12));
+// console.log(sum(12, 12));
+// console.log(sum(12, 13, 12));
+// console.log(sum(12, 12, 12, 12));
+
+const greeting = "Hello";
+console.log(greeting.toLowerCase());
