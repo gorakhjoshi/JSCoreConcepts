05404cc3e3ff11a1d3c3d4a05e2a0727662f0719
Gorakh Raj joshi <gorakh626@gmail.com>
2022-06-02 06:49:27 +0545

    type conversion and coercion

--- script.js
+++ script.js
@@ -1,45 +1,7 @@
 "use strict";
 
-// Function Declaration
-// function sum(a, b) {
-//   console.log(a + b);
-// }
+// conversion
+// let person = Number(prompt("Please any number"));
+// console.log(typeof 105);
 
-// console.log(sum);
-// console.log(myCountry);
-
-// Function Expression
-const sum = function (a, b) {
-  console.log("Running from function expression");
-  console.log(a + b);
-};
-sum(2, 3);
-
-// var myCountry = "Nepal";
-
-// Arrow Function
-// const sum = (a = 0, b = 0, c = 0) => {
-//   return a + b + c;
-// };
-
-// function sum() {
-//   return console.log("Function Declaration");
-// }
-
-// const sum = [1, 2, 3, 4];
-
-// sum(); //Arguments
-
-// console.log(sum);
-
-// Calling functiion
-// Running Function
-// Invoking Function
-
-// console.log(sum(12));
-// console.log(sum(12, 12));
-// console.log(sum(12, 13, 12));
-// console.log(sum(12, 12, 12, 12));
-
-const greeting = "Hello";
-console.log(greeting.toLowerCase());
+console.log("10" + "50" - 10);
